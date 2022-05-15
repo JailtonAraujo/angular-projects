2d128cde06ccd5879d7efb0f37e62942152aaaaa
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-05-15 01:19:22 -0300

    Services

--- angular-started/src/app/components/list-render/list-render.component.ts
+++ angular-started/src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -18,13 +20,18 @@ export class ListRenderComponent implements OnInit {
 
   animalsDatails = '';
 
-  constructor() { }
+  constructor(private listService : ListService ) { }
 
   ngOnInit(): void {
   }
 
   showAge(animal: Animal){
     this.animalsDatails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
+
+  removeAnimal(animal:Animal){
+    console.log("Removando animal!")
+    this.animals = this.listService.remove(this.animals, animal);
   }
   
 }
